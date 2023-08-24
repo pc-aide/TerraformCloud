6597f821aa67f7b67c3a7527b22ab6a40ee14e79
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-08-24 12:45:56 -0400

    Update variables.tf

--- La01-serviceBus/variables.tf
+++ La01-serviceBus/variables.tf
@@ -13,3 +13,31 @@ variable "subscription_id" {
 variable "tenant_id" {
   type = string
 }
+
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+// have a uniq name available
+variable "sbn_name" {
+  type = string
+}
+
+variable "sbn_sku" {
+  type = string
+}
+
+// queue name
+variable "sbn_queue_name" {
+  type = string
+}
+
+// auth queue (SAS Policy)
+variable "sbn_queue_auth_rul_name" {
+  type    = string
+  default = "SenderPolicy"
+}
