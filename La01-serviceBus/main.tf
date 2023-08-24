708f95ad2e36f22f85813708b5473a05c97ecb7c
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-08-24 11:30:53 -0400

    Update main.tf

--- La01-serviceBus/main.tf
+++ La01-serviceBus/main.tf
@@ -1,21 +1,13 @@
-########################################################
-# PROVIDERS
-########################################################
-
 provider "azurerm" {
   version = "<=2.0.0"
 
   subscription_id = var.subscriptionID
-  client_id = var.clientID
-  client_secret = var.clientSecret
-  tenant_id = var.tenantID
+  client_id       = var.clientID
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantID
 
-features {}
+  features {}
 }
-
-########################################################
-# RESOURCE_GROUP
-########################################################
 
 resource "azurerm_resource_group" "rg_name" {
   name     = var.rg_name
